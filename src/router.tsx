9e2847fea9f70a409d3424fccf03a5435ca903cf
Pavel Dorozhkin <pavel.dorozhkin@osinit.com>
2026-03-14 15:43:41 +0300

    ci: add GitHub Pages deployment workflow

--- src/router.tsx
+++ src/router.tsx
@@ -3,10 +3,13 @@ import { ProtectedRoute } from '@/components/ProtectedRoute'
 import { LoginPage } from '@/pages/LoginPage'
 import { ProductsPage } from '@/pages/ProductsPage'
 
-export const router = createBrowserRouter([
-  { path: '/login', element: <LoginPage /> },
-  {
-    element: <ProtectedRoute />,
-    children: [{ path: '/', element: <ProductsPage /> }],
-  },
-])
+export const router = createBrowserRouter(
+  [
+    { path: '/login', element: <LoginPage /> },
+    {
+      element: <ProtectedRoute />,
+      children: [{ path: '/', element: <ProductsPage /> }],
+    },
+  ],
+  { basename: '/aiti-guru' },
+)
